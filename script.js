a5e94c4d67f9fb8afac42c66a81c405160eec2ac
Mudasar Shakil <117173890+mudasarasim@users.noreply.github.com>
2025-06-24 23:34:23 -0700

    latest

--- script.js
+++ script.js
@@ -357,3 +357,17 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+
+// Load header
+fetch('header.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  });
+
+// Load footer
+fetch('footer.html')
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  });
